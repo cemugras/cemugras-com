467b7b78dcb4f85e1d4beecf90360c6639d2c55e
Cem Uğraş <cem@codeshake.io>
2022-12-06 00:14:43 +0300

    one-page implementations.

--- cem-web/src/app/app-routing.module.ts
+++ cem-web/src/app/app-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutPageComponent } from "./about-page/about-page.component";
-import { GamePageComponent } from "./game-page/game-page.component";
 import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
 
-import {TetrisGameComponent} from "./game-page/tetris-game/tetris-game.component";
 
 const routes: Routes = [
-  { path: '', component: GamePageComponent },
-  { path: 'about', component: AboutPageComponent },
+  { path: '', component: AboutPageComponent },
   { path: 'notfound', component: NotFoundPageComponent },
-  { path: 'games/tetris', component: TetrisGameComponent},
   { path: '**', redirectTo: 'notfound' }
 ];
 
--- cem-web/src/app/app.module.ts
+++ cem-web/src/app/app.module.ts
@@ -8,19 +8,13 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatCardModule} from "@angular/material/card";
 
 import { AboutPageComponent } from './about-page/about-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { GamePageComponent } from './game-page/game-page.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
-import { TetrisGameComponent} from "./game-page/tetris-game/tetris-game.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutPageComponent,
-    HomePageComponent,
-    GamePageComponent,
-    NotFoundPageComponent,
-    TetrisGameComponent
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
