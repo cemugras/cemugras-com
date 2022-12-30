a742f9068d7c55d4259a774f424097105dd6ef2c
Cem Uğraş <cemugrs@gmail.com>
2022-12-30 12:26:55 +0300

    Spotify's info added to related UI pages with mobile responsive.

--- cem-web/src/app/contact-page/contact-page.component.ts
+++ cem-web/src/app/contact-page/contact-page.component.ts
@@ -12,9 +12,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {name: 'LinkedIn', url: 'linkedin.com/in/cemugras/', icon: '/assets/sidebar/linkedin-icon.png'},
   {name: 'HackerRank', url: 'hackerrank.com/cemugrs', icon: '/assets/sidebar/hackerrank-icon.png'},
   {name: 'Google Play', url: 'play.google.com/store/apps/developer?id=Cem+Ugras', icon: '/assets/sidebar/googleplay-icon.png'},
+  {name: 'GitHub', url: 'github.com/cemugras', icon: '/assets/sidebar/github-icon.png'},
+  {name: 'Spotify', url: 'open.spotify.com/user/cemugrs', icon: '/assets/sidebar/spotify-icon.png'},
   {name: 'Youtube', url: 'youtube.com/@mzm1187', icon: '/assets/sidebar/youtube-icon.png'},
   {name: 'Steam', url: 'steamcommunity.com/id/cembabbe/', icon: '/assets/sidebar/steam-icon.png'},
-  {name: 'GitHub', url: 'github.com/cemugras', icon: '/assets/sidebar/github-icon.png'},
   {name: 'Donate', url: 'www.buymeacoffee.com/cemugras', icon: '/assets/sidebar/donate-icon.png'}
 ];
 
