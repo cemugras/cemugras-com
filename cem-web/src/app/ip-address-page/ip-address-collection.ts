8d06c77a1fa8eca6dd343ff972df654f0117872b
kafeincugras <cem.ugras@vodafone.com>
2023-04-19 16:27:44 +0300

    My IP page refactoring.

--- cem-web/src/app/ip-address-page/ip-address-collection.ts
+++ cem-web/src/app/ip-address-page/ip-address-collection.ts
@@ -1,22 +1,13 @@
-export class IpClass {
-  constructor(
-    public ip: string
-  ) {}
-}
-
 export class IpGeoLocationClass {
   constructor(
-    public country: string,
-    public countryCode: string,
-    public region: string,
-    public regionName: string,
+    public ip: string,
     public city: string,
-    public zip: string,
-    public lat: string,
-    public lon: string,
-    public timeZone: string,
-    public isp: string,
-    public as: string,
+    public region: string,
+    public country: string,
+    public loc: string,
+    public org: string,
+    public postal: string,
+    public timezone: string,
     public flagUrl: string
   ){}
 }
