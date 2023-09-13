6ae0d2f7bbbac93293b1f0c010190ef1038cee3b
cemugras <cemugrs@gmail.com>
2023-09-13 13:16:37 +0300

    game server ping page design fixes

--- cem-web/src/app/game-server-ping-page/game-server-ping-page.component.ts
+++ cem-web/src/app/game-server-ping-page/game-server-ping-page.component.ts
@@ -16,24 +16,44 @@ Chart.register(...registerables);
 })
 export class GameServerPingPageComponent implements OnInit{
   currentScreenSize!: string;
+
+  // Definition of ping values for charts
   public pingLolEuWest = 0;
   public pingLolEuEast = 0;
   public pingLolUs = 0;
+  public pingCsEuWest = 0;
+  public pingCsEuEast = 0;
+  public pingCsUsWest = 0;
+  public pingCsUsEast = 0;
   
   // Definition of switch statuses
   public lolEuWest = false;
   public lolEuEast = false;
   public lolUs = false;
+  public csEuWest = false;
+  public csEuEast = false;
+  public csUsWest = false;
+  public csUsEast = false;
 
   // Definition of charts
   lolChartEuWest!: DoughnutChart;
   lolChartEuEast!: DoughnutChart;
   lolChartUs!: DoughnutChart;
+  csChartEuWest!: DoughnutChart;
+  csChartEuEast!: DoughnutChart;
+  csChartUsWest!: DoughnutChart;
+  csChartUsEast!: DoughnutChart;
 
   // Lol server url's
   private urlLolEuWest = "https://dynamodb.eu-west-2.amazonaws.com/?random-no-cache=130ee";
   private urlLolEuEast = "https://dynamodb.eu-central-1.amazonaws.com/?random-no-cache=1e155";
   private urlLolUs = "https://dynamodb.us-east-2.amazonaws.com/?random-no-cache=13989";
+
+  // CS server url's
+  private urlCsEuWest = "https://dynamodb.eu-west-2.amazonaws.com/?random-no-cache=1ce68";
+  private urlCsEuCentral = "https://dynamodb.eu-central-1.amazonaws.com/?random-no-cache=13c96";
+  private urlCsUsWest = "https://dynamodb.us-west-1.amazonaws.com/?random-no-cache=1e998";
+  private urlCsUsEast = "https://dynamodb.us-east-1.amazonaws.com/?random-no-cache=17b76";
 
   constructor(private httpClient: HttpClient, private translate: TranslateService, private ref:ChangeDetectorRef) {
     this.currentScreenSize = GlobalFile.screenSize;
@@ -48,6 +68,11 @@ export class GameServerPingPageComponent implements OnInit{
     this.lolChartEuWest = new DoughnutChart('lolChartEuWest');
     this.lolChartEuEast = new DoughnutChart('lolChartEuEast');
     this.lolChartUs = new DoughnutChart('lolChartUs');
+
+    this.csChartEuWest = new DoughnutChart('csChartEuWest');
+    this.csChartEuEast = new DoughnutChart('csChartEuEast');
+    this.csChartUsWest = new DoughnutChart('csChartUsWest');
+    this.csChartUsEast = new DoughnutChart('csChartUsEast');
   }
 
   slideChange(event: MatSlideToggleChange, chartType: Number):void {
@@ -69,6 +94,26 @@ export class GameServerPingPageComponent implements OnInit{
           console.log("[slideChange()] LoL Us -> on.");
           this.lolUs = event.checked;
           this.pingManager(this.urlLolUs, 3);
+          break;
+        case 4: // Cs EU West
+          console.log("[slideChange()] Cs EU West -> on.");
+          this.csEuWest = event.checked;
+          this.pingManager(this.urlCsEuWest, 4);
+          break;
+        case 5: // CS EU Central
+          console.log("[slideChange()] CS EU Central -> on.");
+          this.csEuEast = event.checked;
+          this.pingManager(this.urlCsEuCentral, 5);
+          break;
+        case 6: // CS US West
+          console.log("[slideChange()] CS US West -> on.");
+          this.csUsWest = event.checked;
+          this.pingManager(this.urlCsUsWest, 6);
+          break;
+        case 7: // CS US East
+          console.log("[slideChange()] CS US East -> on.");
+          this.csUsEast = event.checked;
+          this.pingManager(this.urlCsUsEast, 7);
           break;
         default:
           break;
@@ -115,6 +160,58 @@ export class GameServerPingPageComponent implements OnInit{
             this.ref.detectChanges();
           }, 1500);
           break;
+        case 4: // CS EU West
+          console.log("[slideChange()] CS EU West -> off.");
+          this.csEuWest = event.checked;
+
+          setTimeout(() => {
+            console.log("[slideChange()] CS EU West chart resetting.");
+            this.pingCsEuWest = 0;
+            this.csChartEuWest.resetChart();
+            
+            // Refresh html data
+            this.ref.detectChanges();
+          }, 1500);
+          break;
+        case 5: // CS EU East
+          console.log("[slideChange()] CS EU East -> off.");
+          this.csEuEast = event.checked;
+
+          setTimeout(() => {
+            console.log("[slideChange()] CS EU East chart resetting.");
+            this.pingCsEuEast = 0;
+            this.csChartEuEast.resetChart();
+            
+            // Refresh html data
+            this.ref.detectChanges();
+          }, 1500);
+          break;
+        case 6: // CS US West
+          console.log("[slideChange()] CS US West -> off.");
+          this.csUsWest = event.checked;
+
+          setTimeout(() => {
+            console.log("[slideChange()] CS US West chart resetting.");
+            this.pingCsUsWest = 0;
+            this.csChartUsWest.resetChart();
+            
+            // Refresh html data
+            this.ref.detectChanges();
+          }, 1500);
+          break;
+        case 7: // CS US East
+          console.log("[slideChange()] CS US East -> off.");
+          this.csUsEast = event.checked;
+
+          setTimeout(() => {
+            console.log("[slideChange()] CS US East chart resetting.");
+            this.pingCsUsEast = 0;
+            this.csChartUsEast.resetChart();
+            
+            // Refresh html data
+            this.ref.detectChanges();
+          }, 1500);
+          break;
         default:
           break;
       }
@@ -137,6 +234,26 @@ export class GameServerPingPageComponent implements OnInit{
           break;
         case 3:
           if(this.lolUs) {
+            this.pingManager(url, chartType);
+          }
+          break;
+        case 4:
+            if(this.csEuWest) {
+              this.pingManager(url, chartType);
+            }
+            break;
+        case 5:
+          if(this.csEuEast) {
+            this.pingManager(url, chartType);
+          }
+          break;
+        case 6:
+          if(this.csUsWest) {
+            this.pingManager(url, chartType);
+          }
+          break;
+        case 7:
+          if(this.csUsEast) {
             this.pingManager(url, chartType);
           }
           break;
@@ -177,6 +294,26 @@ export class GameServerPingPageComponent implements OnInit{
               this.pingLolUs = Math.trunc(timeElapsed)-2;
               this.lolChartUs.updateChartData(this.pingLolUs);
               break;
+            case 4: // CS EU West
+              console.log("[postPing()] CS EU West updating.");
+              this.pingCsEuWest = Math.trunc(timeElapsed)-2;
+              this.csChartEuWest.updateChartData(this.pingCsEuWest);
+              break;
+            case 5: // CS EU East
+              console.log("[postPing()] CS EU Central updating.");
+              this.pingCsEuEast = Math.trunc(timeElapsed)-2;
+              this.csChartEuEast.updateChartData(this.pingCsEuEast);
+              break;
+            case 6: // CS Us West
+              console.log("[postPing()] CS Us West updating.");
+              this.pingCsUsWest = Math.trunc(timeElapsed)-2;
+              this.csChartUsWest.updateChartData(this.pingCsUsWest);
+              break;
+            case 7: // CS Us East
+              console.log("[postPing()] CS Us East updating.");
+              this.pingCsUsEast = Math.trunc(timeElapsed)-2;
+              this.csChartUsEast.updateChartData(this.pingCsUsEast);
+              break;
             default:
               break;
           }
@@ -200,6 +337,22 @@ export class GameServerPingPageComponent implements OnInit{
       case 3: // LoL Us
         console.log("[resetChart()] LoL Eu West chart resetting.");
         this.lolChartUs.resetChart();
+        break;
+      case 4: // CS EU West
+        console.log("[resetChart()] CS EU West chart resetting.");
+        this.csChartEuWest.resetChart();
+        break;
+      case 5: // CS EU East
+        console.log("[resetChart()] CS EU East chart resetting.");
+        this.csChartEuEast.resetChart();
+        break;
+      case 6: // CS US West
+        console.log("[resetChart()] CS US West chart resetting.");
+        this.csChartUsWest.resetChart();
+        break;
+      case 7: // CS US East
+        console.log("[resetChart()] CS US East chart resetting.");
+        this.csChartUsEast.resetChart();
         break;
       default:
         break;
